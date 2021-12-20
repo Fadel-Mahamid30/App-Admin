8df4e3c04cb2f92368735f07b37d442e627998cd
Fadel Mahamid Ahmad <fadelmahamid107@gmail.com>
2021-12-20 19:48:22 +0700

    update

--- app/Controllers/Register.php
+++ app/Controllers/Register.php
@@ -25,53 +25,19 @@ class Register extends BaseController
 
     public function store()
     {
-        //load helper form and URL
-        helper(['form', 'url']);
-         
-        //define validation
-        $validation = $this->validate([
-            'email' => [
-                'rules'  => 'required|is_unique[TblUser.email]',
-                'errors' => [
-                    'required' => 'Masukkan email anda/Email sudah di pakai.'
-                ]
-            ],
-            'Password'    => [
-                'rules'  => 'required',
-                'errors' => [
-                    'required' => 'Masukkan Password.'
-                ]
-            ],
-            'namaPengguna'    => [
-                'rules'  => 'required',
-                'errors' => [
-                    'required' => 'Masukkan Nama Pengguna.'
-                ]
-            ],
+        $rumahSakit = new \App\Models\UserModel();
+        $rumahSakit->insert([
+            'email' => $this->request->getPOST('email'),
+            'userType' => 'Admin',
+            'userAlias' => '',
+            'namaPengguna' => $this->request->getPOST('namaPengguna'),
+            'Password' => $this->request->getPOST('Password'),
+            'Status' => '0'
         ]);
-
-        if(!$validation) {
-
-            //render view with error validation message
-            return view('admin/Register', [
-                'validation' => $this->validator,
-                'title'     => 'Register',
-            ]);
-
-        } else
-            $rumahSakit = new \App\Models\UserModel();
-		    $rumahSakit->insert([
-			    'email' => $this->request->getPOST('email'),
-			    'userType' => 'Admin',
-			    'userAlias' => '',
-			    'namaPengguna' => $this->request->getPOST('namaPengguna'),
-			    'Password' => $this->request->getPOST('Password'),
-                'Status' => '0'
-		    ]);
-
-            //flash message
-            session()->setFlashdata('Register', 'Post Berhasil Disimpan');
-
-            return redirect()->to(base_url('/'));
-        }
+    
+        //flash message
+        session()->setFlashdata('Register', 'Post Berhasil Disimpan');
+    
+        return redirect()->to(base_url('/'));
+    }
 }
